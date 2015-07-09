ab63164e0a7c3b205f19ec695738d3bd3fa3b3a6
Alex Jones <jonesax@hush.com>
2015-07-09 12:37:52 +0100

    modified to hold open the console window

--- TwitterJam/Program.cs
+++ TwitterJam/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Autofac;
-using LinqToTwitter;
 using TwitterJam.Implementation;
 using TwitterJam.Interfaces;
 
@@ -10,18 +7,22 @@ namespace TwitterJam
 {
     internal class Program
     {
-
         private static void Main(string[] args)
         {
+            var container = new IoC().GetContainer();
 
-            IContainer container = new IoC().GetContainer();
-
-            ITwitterService service = container.Resolve<ITwitterService>();
+            var service = container.Resolve<ITwitterService>();
 
             service.Authorise("evRKMja4psyaHoevCVMPlCbHm", "W72GjBqSZ5fQ6uVDMkMYpoDKfiqwAaOyLNMczZJKdKX3yk35Ev",
-                "3366075040-AtwlMptYENBFVmdDtU8A08EuyoNuMRFAQmZTzzq","ouBcNL2hawU7JUQPjrWJ1Ehai7sh3HiUUycLrXl2E2YB0");
+                "3366075040-AtwlMptYENBFVmdDtU8A08EuyoNuMRFAQmZTzzq", "ouBcNL2hawU7JUQPjrWJ1Ehai7sh3HiUUycLrXl2E2YB0");
 
-            ITwitterStatusFeed timeline = service.FetchTimeLine();
+            var timeline = service.FetchTimeLine();
+
+            timeline.StatusInformation.ForEach(
+                item => Console.WriteLine(item.Text)
+                );
+
+            Console.ReadKey();
         }
     }
 }
